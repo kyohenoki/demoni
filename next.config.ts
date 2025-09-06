bd2e005a85edf70290caff6f6ba8aa146493f0cb
kyohei <kyohei103@outlook.jp>
2025-09-06 09:49:25 +0900

    戻す

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import { withPayload } from '@payloadcms/next/withPayload'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   devIndicators: false
 }
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default nextConfig
